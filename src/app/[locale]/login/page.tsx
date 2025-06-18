f6676d02a917041aaa55cca5bf685b480e60983c
Mokhtar Qaid <67845987+makrabi@users.noreply.github.com>
2025-06-18 13:29:20 +0300

    Update page.tsx

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,18 +1,16 @@
-'use client'; 
+'use client';
 
 import { useTranslations } from 'next-intl';
 import LoginForm from './LoginForm';
 
-// تعريف Props صحيح للمكون العميل (params هو كائن)
 type Props = {
   params: {
     locale: string;
   };
 };
 
-// الدالة لا تحتاج أن تكون async ولا تحتاج await لـ params
 export default function LoginPage({ params: { locale } }: Props) {
-  const t = useTranslations('LoginPage'); // استخدام hook الترجمة هنا
+  const t = useTranslations('LoginPage');
 
   return (
     <div>
